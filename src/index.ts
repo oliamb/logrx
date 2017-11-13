7d0129cc1fd1aad5e30a673b5f81efe9f7b36351
Olivier Amblet <olivier.amblet@pix4d.com>
2017-11-13 23:46:06 +0100

    chore: moved source to src

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { LoggerContext } from './logger-context';
 export { getLogger, addAppenderFactory, configure } from './logger-global-context';
-export { ILogger } from './types';
+export { ILogger, IAppender } from './types';
+export * from './const';
